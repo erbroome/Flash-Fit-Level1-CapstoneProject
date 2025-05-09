7b2d2f9ca445740388c8bfdfa848b089b619158f
erbroome <ellenehall@gmail.com>
2025-05-09 09:54:20 -0500

    added navbar pages

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ let allExercises = [
     "Push-ups", "Squats", "Jumping Jacks", "Lunges",
     "Plank", "Sit-ups", "Mountain Climbers", "Burpees",
     "High Knees", "Butt Kicks", "Bicycle Crunches", "Wall Sit", "Tricep Dips",
-    "Leg Raises", "Russian Twists", "Side Plank", "Glute Bridges",       
+    "Leg Raises", "Russian Twists", "Side Plank", "Glute Bridges", "donkey kicks",
+    "Superman", "Flutter Kicks", "Reverse Crunches", "T-Push-ups", "Skaters", "calf raises", "Hip Thrusts", "Side Lunges", "Jump Squats", "Box Jumps",   
   ];
   
   // Function that runs when button is clicked
@@ -21,11 +22,13 @@ let allExercises = [
     for (let i = 0; i < 4; i++) {
     // Pick a random index from the array
     let randomIndex = Math.floor(Math.random() * allExercises.length);
-      selected.push(allExercises[randomIndex]);
+      if (!selected.includes(allExercises[randomIndex])) {
+        selected.push(allExercises[randomIndex]);
+      }
       console.log("Selected exercise: " + allExercises[randomIndex]);
     }
   
-    // Show the exercises in the HTML
+    // exercises into the HTML
     let output = "<ul>";
     for (let i = 0; i < selected.length; i++) {
       output += "<li>" + selected[i] + "</li>";
